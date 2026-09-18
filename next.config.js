54e89b0313e004fb194b4cf8d53fc0401c961ab4
buanamedia <media.agussiahaan@gmail.com>
2026-09-18 10:12:02 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Memaksa Vercel mengabaikan error TypeScript saat build
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Memaksa Vercel mengabaikan error ESLint saat build
     ignoreDuringBuilds: true,
   },
   swcMinify: true,
