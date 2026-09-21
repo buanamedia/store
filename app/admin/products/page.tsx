2448a24130daa39ab0819be159f02c0c188cd94b
buanamedia <media.agussiahaan@gmail.com>
2026-09-21 07:07:17 +0000

    Update page.tsx

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 export const dynamic = "force-dynamic";
 
 // URL Web App Google Apps Script Anda
-const GAS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbx.../exec"; 
+const GAS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbx23eHNYQIgdnkZckezSsKvvmOaLqBYreIJRiQJbVZEN_71h6cRZO8LUrEskl2riK_G/exec"; 
 
 export default function AdminDashboardPage() {
   const [adminPassword, setAdminPassword] = useState("");
