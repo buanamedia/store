989001d6b6a648ce5e17275aad33e03ba8db6a98
buanamedia <media.agussiahaan@gmail.com>
2026-09-21 07:13:26 +0000

    Update page.tsx

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 export const dynamic = "force-dynamic";
 
 export default function AdminProductsRedirectPage() {
-  const router = Router();
+  const router = useRouter();
 
   useEffect(() => {
     router.replace("/admin");
