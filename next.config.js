a4af985c8396707bae32c11852935cc52c6668e7
buanamedia <media.agussiahaan@gmail.com>
2026-09-21 04:51:50 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        // Menerapkan CORS Headers ke seluruh endpoint API
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+      {
+        // Menerapkan CORS untuk file static widget.js
+        source: "/blogspot/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
